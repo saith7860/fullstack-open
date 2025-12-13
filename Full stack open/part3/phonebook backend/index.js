97c8b95b3729ee3ae371cb77167ec00e3edba8fa
Muhammad Shaheer Zafar <shaheerzafar592@gmail.com>
2025-12-13 18:12:38 +0500

    add env file with  port

--- Full stack open/part3/phonebook backend/index.js
+++ Full stack open/part3/phonebook backend/index.js
@@ -3,9 +3,11 @@ import personsData from './personsData.js';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors'
-const port=3001;
+import dotenv from 'dotenv';
+dotenv.config();
 // console.log(personsData);
 const app=express();
+const PORT=process.env.PORT||3001
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("common"));
@@ -68,7 +70,7 @@ app.delete("/api/persons/:id",(req,res)=>{
 })
 
 //Port Listening on 3000
-app.listen(port,()=>{
-    console.log(`app is listening on port ${port}`);
+app.listen(PORT,()=>{
+    console.log(`app is listening on port ${PORT}`);
     
 })
