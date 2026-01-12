d349c6e5f0ddffb4be4b00451d1fa9fd0a6d393d
saith7860 <shaheerzafar592@gmail.com>
2026-01-12 15:35:05 +0500

    Delete Full stack open/backend/blog application directory

--- Full stack open/backend/blog application/server/routers/blogRouter.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { getBlogs,postBlogs} from '../controllers/blogController.js';
-const router =express.Router();
-router.get('/',getBlogs )
-router.post('/',postBlogs );
-export default router;
